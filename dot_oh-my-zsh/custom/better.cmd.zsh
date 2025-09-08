df8663d70a902e79596d7ab638b97524b101f77a
RyanLincs <ryan.lincs@outlook.com>
2025-09-09 04:19:33 +0800

    添加find命令

--- dot_oh-my-zsh/custom/better.cmd.zsh
+++ dot_oh-my-zsh/custom/better.cmd.zsh
@@ -1,17 +1,17 @@
 ## eza: better ls
 alias ls=eza
 alias ll="ls -lh --group-directories-first -s age --color-scale age --color-scale-mode gradient --time-style relative -u"
+alias la='ll -a'
 
 ## dust: better du
 alias du=dust
 
-## dust: better du
-alias du=dust
+## fd: better find
+alias find=fd
 
 ## rmtrash
 alias del="rmtrash"
 alias trash="rmtrash"
-
 
 # -E 使用现代正则表达式
 alias fd-e='find -E'
