1ceef9cd272dc341e2193148ce24f8ed905e9227
RyanLincs <ryan.lincs@outlook.com>
2025-09-19 00:57:34 +0800

    拆分fortune库

--- dot_oh-my-zsh/custom/fortune.zsh
+++ dot_oh-my-zsh/custom/fortune.zsh
@@ -1,6 +1,9 @@
+# 数据库在 ~/.local/share/fortune/bettercmd
 function fortune_boxed() {
-    fortune $HOME/.local/share/fortune/bettercmd | boxes
+    fortune $HOME/.local/share/fortune/ | boxes -d ansi
 }
 
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd fortune_boxed
+
+# vim: set ft=zsh
