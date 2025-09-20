ff10a5f6e6be59d7ae7f3e44caed559020fc264f
RyanLincs <ryan.lincs@outlook.com>
2025-09-20 10:58:18 +0800

    日常typo和更改

---
+++ dot_oh-my-zsh/custom/git.zsh
@@ -0,0 +1,16 @@
+function git-c-gh() {
+    # 个人github
+    git config user.name "Ryan Lincs"
+    git config user.email "ryan.lincs@outlook.com"
+}
+function git-c-w() {
+    # 工作环境
+    git config user.name "Rili"
+    # git config user.email "ryan.lincs@outlook.com"
+}
+
+function git-c-p() {
+    # 其他个人部署的git
+    git config user.name "Rili"
+    # git config user.email "ryan.lincs@outlook.com"
+}
