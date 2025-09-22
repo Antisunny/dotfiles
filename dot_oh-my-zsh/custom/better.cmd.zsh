8447f6b5179463253da993ae5f20a3d50ea99c72
RyanLincs <ryan.lincs@outlook.com>
2025-09-23 07:40:02 +0800

    添加了原始ls、xh、aria2c

--- dot_oh-my-zsh/custom/better.cmd.zsh
+++ dot_oh-my-zsh/custom/better.cmd.zsh
@@ -1,5 +1,5 @@
 ## eza: better ls
-alias l=ls
+alias l="command ls" # or /bin/ls
 alias ls=eza
 alias ll="ls -lh --group-directories-first -s modified --color-scale modified --color-scale-mode gradient --time-style relative -m"
 alias la='ll -a'
@@ -33,3 +33,7 @@ alias ps="procs"
 
 # 生成随机密码
 alias pwgen32="/usr/local/bin/pwgen -cnyBs -C 32 1"
+
+# curl wget
+alias xh="xh --style=fruity --print=Hh"
+alias aria="aria2c -x 8 -s 8"
