0ea3e7c2eca9f3ae0fd6eb7ec0a424d91a17ad42
RyanLincs <ryan.lincs@outlook.com>
2025-09-25 20:26:41 +0800

    设置fortune为灰色

--- dot_oh-my-zsh/custom/fortune.zsh
+++ dot_oh-my-zsh/custom/fortune.zsh
@@ -1,6 +1,6 @@
 # 数据库在 ~/.local/share/fortune/bettercmd
 function fortune_boxed() {
-    fortune $HOME/.local/share/fortune/merged | boxes -d ansi --color
+    fortune $HOME/.local/share/fortune/merged | boxes -d ansi --color | awk 'BEGIN{print "\033[90m"} {print} END{print "\033[0m"}'
 }
 
 autoload -Uz add-zsh-hook
