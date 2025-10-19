956b56f9d53f297736edac0945f6c40b2fe6344e
RyanLincs <ryan.lincs@outlook.com>
2025-10-20 03:33:16 +0800

    添加macOS自带python的内容

--- dot_oh-my-zsh/custom/lang.python.zsh
+++ dot_oh-my-zsh/custom/lang.python.zsh
@@ -1,6 +1,9 @@
+alias python3-sys='echo macOS自带的Python && /usr/bin/python3'
+alias pip3-sys='echo macOS自带的pip && /usr/bin/pip3'
+alias pip3-cn='echo 使用清华源安装pip包 && pip install -i https://pypi.tuna.tsinghua.edu.cn/simple'
+alias pip3-sys-cn='echo macOS自带的Python[清华源] && /usr/bin/pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple'
 
-alias python3-sys='/usr/bin/python3'
-alias pip3-sys='/usr/bin/pip3'
-
-alias pip3-cn='pip install -i https://pypi.tuna.tsinghua.edu.cn/simple'
-alias pip3-sys-cn='/usr/bin/pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple'
+alias python-sys='echo macOS自带的Python && /usr/bin/python3'
+alias pip-sys='echo macOS自带的pip && /usr/bin/pip3'
+alias pip-cn='echo 使用清华源安装pip包 && pip install -i https://pypi.tuna.tsinghua.edu.cn/simple'
+alias pip-sys-cn='echo macOS自带的Python[清华源] && /usr/bin/pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple'
