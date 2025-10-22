021cec021e1790834731c9ef4e9d9137bda2ab65
RyanLincs <ryan.lincs@outlook.com>
2025-10-23 05:35:36 +0800

    修复java脚本每次都要输入密码的问题

--- dot_oh-my-zsh/custom/lang.java.zsh
+++ dot_oh-my-zsh/custom/lang.java.zsh
@@ -1,10 +1,15 @@
-sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+if [ ! -d /Library/Java/JavaVirtualMachines/openjdk.jdk ]; then
+    sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+fi
 
 export PATH="/usr/local/opt/openjdk/bin:$PATH"
 export CPPFLAGS="-I/usr/local/opt/openjdk/include"
 
 # 切换不同版本的java
 activate-java() {
+    # 当前java版本
+    java --version | head -n 1
+    # 读取用户输入的版本号
     read -r -p "Enter Java version (e.g., 8, 11, 17, 20, 25): " version
     sudo rm /Library/Java/JavaVirtualMachines/openjdk.jdk
     sudo ln -sfn /usr/local/opt/openjdk@$version/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
