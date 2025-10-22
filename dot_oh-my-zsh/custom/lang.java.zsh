6017af2b17b8dc07450e073f9e7c195d9c2949d4
RyanLincs <ryan.lincs@outlook.com>
2025-10-23 05:05:10 +0800

    nextflow基于java，配置了java

--- dot_oh-my-zsh/custom/lang.java.zsh
+++ dot_oh-my-zsh/custom/lang.java.zsh
@@ -1,15 +1,13 @@
+sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/openjdk/include"
+
 # 切换不同版本的java
 activate-java() {
-    if [[ "$1" == "11" ]]; then
-        sudo rm /Library/Java/JavaVirtualMachines/openjdk.jdk
-        sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-        java --version
-    elif [[ "$1" == "18" ]]; then
-        sudo rm /Library/Java/JavaVirtualMachines/openjdk.jdk
-        sudo ln -sfn /usr/local/opt/openjdk@18/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-        java --version
-    else
-        echo "not supported arguent"
-    fi
+    read -r -p "Enter Java version (e.g., 8, 11, 17, 20, 25): " version
+    sudo rm /Library/Java/JavaVirtualMachines/openjdk.jdk
+    sudo ln -sfn /usr/local/opt/openjdk@$version/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+    java --version
 }
 
