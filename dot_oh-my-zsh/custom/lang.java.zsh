7e90e1514995472a186e9bf21f33d2ad99cda529
RyanLincs <ryan.lincs@outlook.com>
2025-10-23 05:47:08 +0800

    通过版本判断java是否需要重新链接

--- dot_oh-my-zsh/custom/lang.java.zsh
+++ dot_oh-my-zsh/custom/lang.java.zsh
@@ -1,4 +1,7 @@
-if [ ! -d /Library/Java/JavaVirtualMachines/openjdk.jdk ]; then
+let sys_version=$(/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/bin/java --version | head -n 1 | awk '{print $2}' | cut -d'.' -f1) # 只获取大版本号
+let opt_version=$(/usr/local/opt/openjdk/bin/java --version | head -n 1 | awk '{print $2}' | cut -d'.' -f1)
+if [ "$sys_version" -ne "$opt_version" ]; then
+    echo "Updating System Java version from $sys_version to $opt_version"
     sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 fi
 
@@ -8,11 +11,11 @@ export CPPFLAGS="-I/usr/local/opt/openjdk/include"
 # 切换不同版本的java
 activate-java() {
     # 当前java版本
-    java --version | head -n 1
+    /usr/bin/java --version | head -n 1
     # 读取用户输入的版本号
     read -r -p "Enter Java version (e.g., 8, 11, 17, 20, 25): " version
     sudo rm /Library/Java/JavaVirtualMachines/openjdk.jdk
     sudo ln -sfn /usr/local/opt/openjdk@$version/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-    java --version
+    /usr/bin/java --version | head -n 1
 }
 
