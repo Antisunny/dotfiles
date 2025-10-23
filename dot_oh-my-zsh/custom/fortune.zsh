6a4fd7ac70c7e3b9d56ccf93d3b918e0569a2e08
RyanLincs <ryan.lincs@outlook.com>
2025-10-23 12:28:31 +0800

    删除boxes框上下的空行

--- dot_oh-my-zsh/custom/fortune.zsh
+++ dot_oh-my-zsh/custom/fortune.zsh
@@ -1,9 +1,9 @@
 # 数据库在 ~/.local/share/fortune/bettercmd
 function fortune_boxed() {
-    fortune $HOME/.local/share/fortune/merged | boxes -d ansi --color | awk 'BEGIN{print "\033[90m"} {print} END{print "\033[0m"}'
+    fortune $HOME/.local/share/fortune/merged | boxes -d ansi --color | awk 'BEGIN { printf "%s", "\033[38;5;196m" } { printf "%s\n", $0 } END { printf "%s", "\033[0m" }'
 }
 
-autoload -Uz add-zsh-hook
+autoload -Uz add-zsh-hook d
 add-zsh-hook precmd fortune_boxed
 
 # vim: set ft=zsh
